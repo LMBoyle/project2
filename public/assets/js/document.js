a53daf2afa77f9e491312b50ff0076c58158ff88
LMBoyle <boylelukem@gmail.com>
2019-10-18 00:04:32 -0400

    added if else for modal title

--- public/assets/js/document.js
+++ public/assets/js/document.js
@@ -2,5 +2,14 @@
 
 $('.addForm').on('click', function (event) {
   event.preventDefault();
+  const docType = $(this).attr('data-docType');
+  const modalTitle = $('#newDocType');
+  if (docType === 'Other') {
+    modalTitle.removeAttr('value');
+    modalTitle.removeAttr('disabled');
+  } else {
+    modalTitle.attr('value', docType);
+    modalTitle.attr('disabled', 'disabled');
+  }
   $('#formInfo').modal('show');
 });
